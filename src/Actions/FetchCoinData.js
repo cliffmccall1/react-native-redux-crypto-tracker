679b861d33bbcaaca962ba3fa6c6ed9875af0982
Cliff McCall <cliff.mccall1@gmail.com>
2018-09-08 22:42:39 -0500

    correction to package.json

--- src/Actions/FetchCoinData.js
+++ src/Actions/FetchCoinData.js
@@ -13,10 +13,13 @@ export default function FetchCoinData() {
     return axios
       .get(`${apiBaseURL}/v1/ticker/?limit=10`)
       .then(res => {
-        dispatch({ type: FETCHING_COIN_DATA_SUCCESS, payload: res.data });
+        return dispatch({
+          type: FETCHING_COIN_DATA_SUCCESS,
+          payload: res.data
+        });
       })
       .catch(err => {
-        dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: err.data });
+        return dispatch({ type: FETCHING_COIN_DATA_FAIL, payload: err });
       });
   };
 }
